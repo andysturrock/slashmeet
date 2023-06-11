883b488f7a57cf00d52e8be710a5f131cfbc80dc
Andy Sturrock <8336404+andysturrock@users.noreply.github.com>
2023-06-11 10:45:37 +0100

    Verify request is from Slack in AWS version

---
+++ function-src/ts-src/aws/verifySlackRequest.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {verifySlackRequest as _verifySlackRequest} from '@slack/bolt';
+import {SlackRequestVerificationOptions} from '@slack/bolt/dist/receivers/verify-request';
+import {APIGatewayProxyEventHeaders} from 'aws-lambda';
+
+// TODO - make this generic across AWS and GCP
+export function verifySlackRequest(headers: APIGatewayProxyEventHeaders, body: string) {
+  const signingSecret = process.env.SLACK_SECRET;
+  if(!signingSecret) {
+    throw new Error("Missing env var SLACK_SECRET");
+  }
+
+  let x_slack_signature = headers['X-Slack-Signature'];
+  if(!x_slack_signature) {
+    throw new Error("Missing X-Slack-Signature header");
+  }
+  if(Array.isArray(x_slack_signature)) {
+    x_slack_signature = x_slack_signature[0];
+  }
+
+  let x_slack_request_timestamp = headers['X-Slack-Request-Timestamp'];
+  if(!x_slack_request_timestamp) {
+    throw new Error("Missing X-Slack-Request-Timestamp header");
+  }
+  if(Array.isArray(x_slack_request_timestamp)) {
+    x_slack_request_timestamp = x_slack_request_timestamp[0];
+  }
+  const number_x_slack_request_timestamp = parseInt(x_slack_request_timestamp as string);
+
+  const slackRequestVerificationOptions: SlackRequestVerificationOptions = {
+    signingSecret: signingSecret,
+    body: body,
+    headers: {
+      'x-slack-signature': x_slack_signature as string,
+      'x-slack-request-timestamp': number_x_slack_request_timestamp
+    }
+  };
+
+  // Throws an exception with details if invalid.
+  _verifySlackRequest(slackRequestVerificationOptions);
+}
