1c00306403033076acb638b4066d308025f540e9
Andy Sturrock <github@sturrock.org>
2023-07-11 22:39:36 +0100

    Add meeting options parsing (#4)
    * Interim commit
    
    * Parsing to datetime working.  Needs cleaning up.  Interim commit.
    
    * Working version in AWS.  Bug in meeting start time due to DST.
    
    * Make fields non-optional
    
    * Use user's timezone to set meeting times correctly

--- function-src/ts-src/generateGoogleMeetURLBlocks.ts
+++ function-src/ts-src/generateGoogleMeetURLBlocks.ts
@@ -1,11 +1,73 @@
 import {Auth, calendar_v3, google} from 'googleapis';
+import {MeetingOptions, parseMeetingArgs} from './parseMeetingArgs';
+import {WebClient, LogLevel} from "@slack/web-api";
+import util from 'util';
 
-export async function generateGoogleMeetURLBlocks(oauth2Client: Auth.OAuth2Client) {
+export async function generateGoogleMeetURLBlocks(oauth2Client: Auth.OAuth2Client, meetingArgs: string, userId: string) {
+  // Give a default name for the meeting if not provided.
+  if(meetingArgs == '') {
+    meetingArgs = 'slashmeet';
+  }
+  let meetingOptions: MeetingOptions;
+  try {
+    meetingOptions = parseMeetingArgs(meetingArgs, new Date());
+  } catch (error) {
+    const blocks = {
+      response_type: 'in_channel',
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Error parsing meeting parameters.`
+          }
+        }
+      ]
+    };
+    return blocks;
+  }
+
+  // User will be specifying meetings times in their local timezone, whereas AWS always uses UTC.
+  // So grab the timezone and we can specify it to Google API later.
+  let timeZone = '';
+  try {
+    const botUserOauthToken = process.env.BOT_USER_OAUTH_TOKEN;
+    if(!botUserOauthToken) {
+      throw new Error("Missing env var BOT_USER_OAUTH_TOKEN");
+    }
+
+    const client = new WebClient(botUserOauthToken, {
+      logLevel: LogLevel.INFO
+    });
+    const result = await client.users.info({
+      user: userId
+    });
+    if(!result.user?.tz) {
+      throw new Error("Cannot get timezone from user object");
+    }
+    timeZone = result.user.tz;
+  }
+  catch (error) {
+    console.error(`Error: ${util.inspect(error)}`);
+    const blocks = {
+      response_type: 'in_channel',
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Error discovering client timezone.`
+          }
+        }
+      ]
+    };
+    return blocks;
+  }
 
   const calendar = google.calendar('v3');
   google.options({auth: oauth2Client});
 
-  // requestId must be different each time, so just use time in milliseconds.s
+  // requestId must be different each time, so just use time in milliseconds.
   const requestId = Date.now.toString();
   const createRequest: calendar_v3.Schema$CreateConferenceRequest = {
     requestId: requestId
@@ -13,16 +75,22 @@ export async function generateGoogleMeetURLBlocks(oauth2Client: Auth.OAuth2Clien
   const conferenceData: calendar_v3.Schema$ConferenceData = {
     createRequest: createRequest
   };
-  const hourInMillis = 1000 * 60 * 60;
+
+  // Convert time to RFC3339 format, without the Z bit as we will specify the timezone.
+  const startDate = meetingOptions.startDate?.toISOString().substring(0, 19);
+  const endDate = meetingOptions.endDate?.toISOString().substring(0, 19);
+
   const event: calendar_v3.Schema$Event = {
     conferenceData: conferenceData,
-    summary: '/meet',
+    summary: meetingOptions.name,
     description: 'Generated /meet event from Slack',
     start: {
-      dateTime: new Date(Date.now()).toISOString()
+      dateTime: startDate,
+      timeZone: timeZone
     },
     end: {
-      dateTime: new Date(Date.now() + hourInMillis).toISOString()
+      dateTime: endDate,
+      timeZone: timeZone
     }
   };
   const meetingParams: calendar_v3.Params$Resource$Events$Insert = {
