f3c49049b82e04c756dcbcd08ef051a2dda725a2
Andy Sturrock <8336404+andysturrock@users.noreply.github.com>
2024-02-07 21:47:58 +0000

    Handle AAD auth redirect and getting refresh token

---
+++ lambda-src/ts-src/generateAADAuthBlocks.ts
@@ -0,0 +1,70 @@
+import crypto from 'crypto';
+import {State, putState} from './stateTable';
+import {AuthorizationUrlRequest, ConfidentialClientApplication, CryptoProvider, ResponseMode} from '@azure/msal-node';
+import {aadScopes} from './aadConfig';
+
+/**
+ * Generate a button for Google login.
+ * CSRF replay attacks are mitigated by using a nonce as the state param in the redirect URL.
+ * The state is the primary key to the info in the SlashMeet_State table which is then queried in the redirect handler.
+ * @param confidentialClientApplication initialised MSAL ConfidentialClientApplication object
+ * @param slack_user_id Slack user id for the user signing in
+ * @param response_url Response URL for use in the redirect handler to send messages to the Slack user
+ * @returns blocks containing the "Sign in to Google" button
+ */
+export async function generateAADAuthBlocks(confidentialClientApplication: ConfidentialClientApplication, redirectUri: string, slack_user_id: string, response_url: string) {
+  const cryptoProvider = new CryptoProvider();
+  const {verifier, challenge} = await cryptoProvider.generatePkceCodes();
+  // Using a nonce for the state mitigates CSRF attacks.
+  const nonce = crypto.randomBytes(16).toString('hex');
+  const state: State = {
+    nonce,
+    slack_user_id,
+    response_url,
+    verifier
+  };
+  await putState(nonce, state);
+
+  const authorizationUrlRequest: AuthorizationUrlRequest = {
+    state: nonce,
+    scopes: aadScopes,
+    redirectUri,
+    responseMode: ResponseMode.FORM_POST,
+    codeChallenge: challenge,
+    codeChallengeMethod: 'S256',
+    prompt: 'consent'
+  };
+  const url = await confidentialClientApplication.getAuthCodeUrl(authorizationUrlRequest);
+  console.log(`URL: ${url}`);
+
+  const blocks = {
+    "blocks": [
+      {
+        type: "section",
+        fields: [
+          {
+            type: "plain_text",
+            text: "Sign in to Microsoft"
+          }
+        ]
+      },
+      {
+        type: "actions",
+        block_id: "signInButton",
+        elements: [
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Sign in to Microsoft"
+            },
+            url,
+            style: "primary",
+            action_id: 'microsoftSignInButton'
+          }
+        ]
+      }
+    ]
+  };
+  return blocks;
+}
---
+++ lambda-src/ts-src/handleAADAuthRedirect.ts
@@ -0,0 +1,147 @@
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
+import {generateLoggedInHTML} from './generateLoggedInHTML';
+import {saveToken} from './tokenStorage';
+import {getSecretValue} from './awsAPI';
+import {deleteState, getState} from './stateTable';
+import util from 'util';
+import querystring from 'querystring';
+import {AuthenticationResult, AuthorizationCodePayload, AuthorizationCodeRequest, ConfidentialClientApplication, Configuration, LogLevel} from '@azure/msal-node';
+import {RefreshTokenEntity} from '@azure/msal-common';
+import {aadScopes} from './aadConfig';
+
+export async function handleAADAuthRedirect(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+  try {
+    console.log(`event: ${util.inspect(event, true, undefined)}`);
+
+    if(!event.body) {
+      throw new Error("Missing event body");
+    }
+    type Params = {
+      code: string,
+      client_info: string,
+      state: string,
+      session_state: string
+    };
+    const params = querystring.parse(event.body) as Params;
+    console.log(`params: ${util.inspect(params, true, undefined)}`);
+
+    const nonce = params.state;
+    const state = await getState(nonce);
+    if(!state) {
+      throw new Error("Missing state.  Are you a cyber criminal trying a CSRF replay attack?");
+    }
+    console.log(`retrieved state: ${util.inspect(state, true, undefined)}`);
+    await deleteState(nonce);
+
+    const aadClientId = await getSecretValue('SlashMeet', 'aadClientId');
+    const aadTenantId = await getSecretValue('SlashMeet', 'aadTenantId');
+    const aadClientSecret = await getSecretValue('SlashMeet', 'aadClientSecret');
+    const slashMeetUrl = await getSecretValue('SlashMeet', 'slashMeetUrl');
+    const aadRedirectUri = `${slashMeetUrl}/aad-oauth-redirect`;
+    const msalConfig: Configuration = {
+      auth: {
+        clientId: aadClientId,
+        authority: `https://login.microsoftonline.com/${aadTenantId}`,
+        clientSecret: aadClientSecret
+      },
+      system: {
+        loggerOptions: {
+          logLevel: LogLevel.Verbose,
+          loggerCallback: (level, message, containsPii) => {
+            if(containsPii) {
+              return;
+            }
+            switch(level) {
+            case LogLevel.Error:
+              console.error(message);
+              return;
+            case LogLevel.Info:
+              console.info(message);
+              return;
+            case LogLevel.Verbose:
+              console.debug(message);
+              return;
+            case LogLevel.Warning:
+              console.warn(message);
+              return;
+            }
+          },
+          piiLoggingEnabled: false
+        },
+      }
+    };
+    const confidentialClientApplication = new ConfidentialClientApplication(msalConfig);
+
+    const authorizationCodeRequest: AuthorizationCodeRequest = {
+      scopes: aadScopes,
+      redirectUri: aadRedirectUri,
+      code: params.code,
+      codeVerifier: state.verifier,
+      state: nonce
+    };
+    const authorizationCodePayload: AuthorizationCodePayload = {
+      ...params
+    };
+    const tokenResponse: AuthenticationResult = await confidentialClientApplication.acquireTokenByCode(authorizationCodeRequest, authorizationCodePayload);
+    // MSAL doesn't give you the refresh_token directly in the AuthenticationResult above.
+    // See https://github.com/AzureAD/microsoft-authentication-library-for-js/issues/2155
+    // On the other hand it doesn't provide a very good mechanism to store the cache when there could
+    // be multiple accounts.  So just get hold of the refresh token from its internals and we'll use that.
+    const account = tokenResponse.account;
+
+    const tokens = confidentialClientApplication.getTokenCache().getKVStore();
+    type Token = {
+      credentialType: "RefreshToken" | "AccessToken" | "IdToken",
+      secret: string
+    };
+    let refreshToken: string | null = null;
+    for(const tokenKey in tokens) {
+      const tokenValue = tokens[tokenKey] as Token;
+      if(tokenValue.credentialType === "RefreshToken") {
+        refreshToken = tokenValue.secret;
+        break;
+      }
+    }
+    if(!refreshToken) {
+      throw new Error("Failed to get refresh token from AAD Entra authentication service.");
+    }
+    console.log(`refreshToken: ${refreshToken}`);
+    // TODO need to have slack id to gcal token and slack id to aad token tables separately
+    // await saveToken(refreshToken, state.slack_user_id);
+
+    const html = generateLoggedInHTML("Microsoft");
+    const result: APIGatewayProxyResult = {
+      body: html,
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html',
+      }
+    };
+
+    return result;
+  }
+  catch (error) {
+    console.error(error);
+
+    const html = `
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Authentication Failure</h1>
+<p>There was an error.  Please check the logs.</p>
+
+</body>
+</html>
+  `;
+
+    const result: APIGatewayProxyResult = {
+      body: html,
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html',
+      }
+    };
+    return result;
+  }
+}
