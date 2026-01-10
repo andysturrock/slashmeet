e8d4ee512dea37c98b3979f8afb20e5ce40f49b2
Andy Sturrock <8336404+andysturrock@users.noreply.github.com>
2026-01-10 17:23:38 +0000

    chore: monorepo cleanup and restructuring into packages directory

--- cdk/jest.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  // roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
-};
---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+process.env.TZ = 'Etc/UTC';
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts', '**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/packages/.*/dist/', '/packages/.*/cdk.out/'],
+  modulePathIgnorePatterns: ['<rootDir>/packages/cdk/dist', '<rootDir>/packages/lambda-src/dist'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
